08691c2f1ada7a844bd0a54a1d6e2f49b33f0563
moblapps22 <moblapps22@gmail.com>
2020-03-12 17:07:50 +0530

    Updates

---
+++ precache-manifest.0e0c2a95962c6e4e61b88ac1fb2fb793.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741d6344a182cc31b05c2c4d392ea8a5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "11527029f83ac839afed",
+    "url": "/my-app/static/css/main.011438ea.chunk.css"
+  },
+  {
+    "revision": "4afdc0ec2e2b936a1227",
+    "url": "/my-app/static/js/2.8fa138d7.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/my-app/static/js/2.8fa138d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11527029f83ac839afed",
+    "url": "/my-app/static/js/main.1bfc48c8.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "246ea86865738b7436bc8401493e628f",
+    "url": "/my-app/static/media/cover.246ea868.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/my-app/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
